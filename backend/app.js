5a992c29eaa506644bae53ba2ac602bd4661ee25
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-07-14 11:52:50 +0545

    Refactor server initialization and add MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,14 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 
-dotenv.config({ quiet: true });
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/health", (req, res) => {
   res.send("Health check passed!");
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
 import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
+
 import app from "./app.js";
+import connectDB from "./config/db-config.js";
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(`Error starting server: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 process.on("SIGINT", () => {
   console.log("Shutting down server...");
