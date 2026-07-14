cf0b5edc26dd25bfa54aea213519e1d3295ff9e2
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-07-14 12:25:49 +0545

    Reorder middleware usage to ensure error handling is applied after route definitions

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,14 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(errMiddleware);
 
 app.get("/health", (req, res) => {
   res.send("Health check passed!");
 });
+
+
+
+
+app.use(errMiddleware);
 
 export default app;
