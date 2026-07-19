39b436886ce9b31c1636e32727bab007999d0d13
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-07-19 18:50:45 +0545

    fix: configure CORS to allow requests from localhost with credentials

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,12 @@ import errorHandler from "./middlewares/error.middleware.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
